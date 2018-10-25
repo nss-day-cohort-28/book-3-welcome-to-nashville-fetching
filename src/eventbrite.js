3387c7526f376958c4a69d830bbfbeb5854bd54f
Daniel Combs <danielcombs@Daniels-MacBook-Pro.local>
2018-10-25 15:55:59 -0500

    merging with master

--- src/eventbrite.js
+++ src/eventbrite.js
@@ -1,6 +1,7 @@
 let eventArray = []
 let freeArray =[]
 let moneyArray = []
+let 
 
 fetch("https://www.eventbriteapi.com/v3/events/search/?location.address=Nashville&expand=venue&sort_by=date", {
     headers: {
@@ -10,10 +11,10 @@ fetch("https://www.eventbriteapi.com/v3/events/search/?location.address=Nashvill
     .then( (events) => events.json())
     .then( (parsedEvents) => {
         eventArray = parsedEvents.events
-        // console.log(eventArray)
+        console.log(eventArray)
 
         for (let i = 0; i < eventArray.length; i++) {
-        
+        // If else statement that creates Objects with the Key Components of Name and website addresses for the object, and populates the free and money arrays
             if (eventArray[i].is_free) { 
                 let obj = {}
                 obj.name= (eventArray[i].name.text) 
@@ -26,14 +27,18 @@ fetch("https://www.eventbriteapi.com/v3/events/search/?location.address=Nashvill
             moneyArray.push(obj)
 
         }
-    }
-    // console.table(moneyArray)
-    
-    // for ( i = 0; i < 5 ; i ++){
-    //     console.log(moneyArray[i])
-    //     }
+        
+        }
+        // Array of events that cost money
+        for ( i = 0; i < 5 ; i ++){
+            console.log(moneyArray[i])
+        }
+
+        // Array of events that are free
         for(i = 0; i < 5; i++){
             console.log(freeArray[i])
         }
+        
+
     })
 
