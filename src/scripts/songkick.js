ded49f70945520a2f1a97aeb1fda24a068ed73c6
Nolan-Little <little.nolan@gmail.com>
2018-10-27 14:03:50 -0500

    updated songkickAPI fetch concerts method and added event listener callbacks

--- src/scripts/songkick.js
+++ src/scripts/songkick.js
@@ -11,35 +11,35 @@ let day = today.getDate()
 let fullDate = `${year}-${month}-${day}`
 
 
-
+let eventList=[]
+let concertArray = []
+let concertContainer = document.getElementById("concert-list")
 // object that has a method to return all of the concerts in nashville metroID area happening "today"
 songkickAPI = {
-  fetchConcerts() {
-    return fetch(`https://api.songkick.com/api/3.0/metro_areas/11104/calendar.json?min_date=${fullDate}&max_date=${fullDate}&per_page=50&apikey=U7F3aXQFBYvzB9Uz`)
-      .then((concerts) => concerts.json())
-      .then((resultsObj) => {
-        let concertArray = []
-        concertArray = resultsObj.resultsPage.results.event
-        return concertArray
+  fetchConcerts(venueID) {
+    fetch(`https://api.songkick.com/api/3.0/metro_areas/11104/calendar.json?min_date=${fullDate}&max_date=${fullDate}&per_page=50&apikey=U7F3aXQFBYvzB9Uz`)
+    .then((concerts) => concerts.json())
+    .then((resultsObj) => {
+      return concertArray = resultsObj.resultsPage.results.event
+    })
+    .then((concertVenueArray) => {
+      concertVenueArray.forEach((event) => {
+        if (event.venue.id === venueID) {
+          let eventObj ={}
+          eventObj.name = event.displayName
+          eventObj.descriptor = `<a href=${event.uri}>Go To Concert Info</a>`
+          eventList.push(eventObj)
+        }
+        })
+      // call element factory with the array that I've created but inside my then statement
+      let uniqueUl = elementfactory(eventList, "concert-ul")
+      concertContainer.appendChild(uniqueUl)
       })
   }
 }
-let concertContainer = document.querySelector("#concert-list")
-let eventList=[]
-let showByVenue = (venueID) => {
-  songkickAPI.fetchConcerts().then((array) => {
-    array.forEach((event) => {
-      if (event.venue.id === venueID) {
-        let eventObj ={}
-        eventObj.name = event.displayName
-        eventObj.descriptor = `<a href=${event.uri}>Go To Concert Info</a>`
-        eventList.push(eventObj)
-      }
-    })
-    // call element factory with the array that I've created but inside my then statement
-    elementfactory(eventList, "concert-ul");
-  })
-}
+
+
+
 
 // create ul to append
 
